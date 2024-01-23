5620be824aac90c3a6508ba7760fd8edc43b85c9
bunrieucuaa <ann71576@gmail.com>
2024-01-23 15:33:14 +0700

    feat: learn about list ,map and set data types #3

--- dart_beginner/bin/example.dart
+++ dart_beginner/bin/example.dart
@@ -30,6 +30,15 @@ void main(List<String> args) {
   /// const tuoiGia1 = tuoiGia;
   //cant be used
 
+  const myList = [1, 2, 3];
+  // invalid
+  // myList.add(5);
+  // print(myList);
+
+  final yourList = [1, 2, 3];
+  yourList.add(4);
+  print(yourList);
+
   //************************************************************************* */
 
   //var thì có thể thay đổi được giá trị sau khi khai báo và
@@ -64,4 +73,59 @@ void main(List<String> args) {
   /// constants cannot be assigned a non-constant value
   /// so this is an invalid operation
   // const age2 = age;
+
+//************************************************************************* */
+
+  ///Int
+  const int myAge = 20;
+  const myBrotherAge = 15;
+
+//************************************************************************* */
+  late final myValue = 10;
+  print(myValue);
+
+  late final yourValue = getValue();
+  print('We are here');
+  print(yourValue);
+
+  // expectations:
+  // getValue called
+  // We are here
+  // 10
+
+  const int someInteger = 10;
+  print(someInteger);
+  const double someDouble = 10.5;
+  print(someDouble);
+  const String someString = 'Hello';
+  print(someString);
+  const bool someBoolean = true;
+  print(!someBoolean);
+
+  ///Kiểu dữ liệu List trong Dart là một cấu trúc để lưu trữ một danh sách có thứ tự
+  ///các phần tử, trong đó các phần tử có thể trùng lặp và có thể là các kiểu dữ liệu khác nhau
+  const List<int> someList = [1, 2, 3];
+  print(someList);
+
+  ///Dart Map là một đối tượng lưu trữ dữ liệu dưới dạng một cặp khóa-giá trị.
+  ///Mỗi giá trị được liên kết với khóa của nó và nó được sử dụng để truy cập giá trị tương ứng của nó.
+  /// Cả khóa và giá trị đều có thể là bất kỳ loại nào.
+  /// Trong Map, mỗi khóa phải là duy nhất, nhưng cùng một giá trị có thể xuất hiện nhiều lần
+  const Map<String, String> someMap = {'foo': 'bar'};
+  print(someMap['foo']);
+
+  ///Set là tập hợp không có thứ tự của các giá trị khác nhau của cùng một loại.
+  /// Nó có nhiều chức năng, giống như một mảng, nhưng nó không có thứ tự.
+  /// Set không cho phép lưu trữ các giá trị trùng lặp. Tập hợp phải chứa các giá trị duy nhất.
+  const Set<int> someSet = {1, 2, 3};
+  print(someSet.length);
+  const dynamic someNull = null;
+  print(someNull);
+  const Symbol someSymbol = #someNull;
+  print(someSymbol);
+}
+
+int getValue() {
+  print('getValue called');
+  return 10;
 }
