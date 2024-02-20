6f867f368d7f874959ba3330a0f26cbdbf0276fe
bunrieucuaa <ann71576@gmail.com>
2024-02-20 21:17:58 +0700

    feat: write unit_testing #7

---
+++ operators_in_dart/bin/ex1.dart
@@ -0,0 +1,6 @@
+void main(List<String> arguments) {
+  const age1 = 64;
+  const age2 = 64;
+
+  print(age1 + age2);
+}
--- operators_in_dart/bin/operators_in_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:operators_in_dart/operators_in_dart.dart' as operators_in_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${operators_in_dart.calculate()}!');
-}
